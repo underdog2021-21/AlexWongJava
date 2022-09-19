86f7a2879e86bd2446640d9cb2bbcabff461dd74
AlexWong <504243054@qq.com>
2022-09-19 20:05:43 +0800

    2021.0617

--- heima-leadnews/heima-leadnews-search/src/main/java/com/heima/search/config/ElasticSearchConfig.java
+++ heima-leadnews/heima-leadnews-search/src/main/java/com/heima/search/config/ElasticSearchConfig.java
@@ -1,10 +1,12 @@
 package com.heima.search.config;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
 import lombok.Data;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,10 +20,11 @@ public class ElasticSearchConfig {
     private Integer port;
 
     @Bean
-    public RestHighLevelClient client(){
-
-        return new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost(host,port)));
+    public ElasticsearchClient client() {
+        RestClient restClient = RestClient.builder(
+                new HttpHost(host, port)).build();
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+        return new ElasticsearchClient(transport);
     }
 }
---
+++ heima-leadnews/heima-leadnews-search/src/main/java/com/heima/search/service/impl/ApUserSearchServiceImpl.java
@@ -0,0 +1,13 @@
+package com.heima.search.service.impl;
+
+import com.heima.model.article.dtos.ArticleDto;
+
+import java.util.List;
+
+public class ApUserSearchServiceImpl implements com.heima.search.service.ApUserSearchService {
+
+    @Override
+    public void creatIndexBatch(List<ArticleDto> articleDtoList) {
+
+    }
+}
